535cd83d56c06dedde747d2172f40a3ddd9c25cf
will-baker <will.baker3@icloud.com>
2024-08-03 17:35:44 +1200

    setup openai api connection

--- src/components/ChatBubble.js
+++ src/components/ChatBubble.js
@@ -5,7 +5,7 @@ const ChatBubble = ({ message, sender }) => {
     return (
         <div className={`chat ${sender === 'user' ? 'chat-end' : 'chat-start'}`}>
             <div className={`chat-bubble  ${sender === 'user' ? 'chat-bubble-primary' : ''}`}>
-                Cat {message}
+                {message}
             </div>
         </div>
         
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import ChatInput from './ChatInput';
 import ChatBubble from './ChatBubble';
+import OpenAI from 'openai';
+//require('dotenv').config();
+
+const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAI_SECRET_KEY, dangerouslyAllowBrowser: true});
+
+//openai.apiKey = process.env.REACT_APP_OPENAI_SECRET_KEY;
 
 function ChatWindow() {
   const [messages, setMessages] = useState([]);
 
   // Function to handle new messages
-  const handleNewMessage = (msg) => {
-    setMessages([...messages, { text: msg, sender: 'user' }]);
-    // Call API and add response to messages
+  const handleNewMessage = async (msg) => {
+    setMessages((prevMessages) => [...prevMessages, { text: msg, sender: 'user' }]);
+  
+    const completion = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        { 'role': 'system', 'content': 'You are a helpful assistant.' },
+        { 'role': 'user', 'content': msg },
+      ],
+    });
+  
+    const aiMessage = completion.choices[0].message.content;
+    console.log(aiMessage);
+  
+    setMessages((prevMessages) => [...prevMessages, { text: aiMessage, sender: completion.model }]);
   };
 
   return (
